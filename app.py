0d5541cbca668798c7cb42dcc34d23ebf683d2d9
EweVitor <ifalinformatica18@gmail.com>
2021-07-24 15:09:51 -0300

    teste8

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ app = Flask(__name__, template_folder=TEMPLATE, static_folder=STATIC)
 @app.route('/')
 def index():
     nome= 'ewerton'
+    mostrarVideos = True
     lista =['https://www.youtube.com/embed/d35pHYwgVpg','https://www.youtube.com/embed/VTSIrwHUZ-M']
-    return render_template('index.html', nomeHtml = nome, lista = lista)
+    return render_template('index.html', nomeHtml = nome, lista = lista,mostrarVideos = mostrarVideos)
 
 #app.run(host='0.0.0.0', port=5000)
