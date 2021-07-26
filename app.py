2adb28ba13b6ae7f667a042367529305bdd8cb7d
EweVitor <ifalinformatica18@gmail.com>
2021-07-25 22:43:13 -0300

    teste9

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask , render_template
 
 
-TEMPLATE ='./template'
+TEMPLATES ='./template'
 STATIC = './static'
 
-app = Flask(__name__, template_folder=TEMPLATE, static_folder=STATIC)
-
-
+app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
 @app.route('/')
+def olaMundo():
+    return 'ola mundo'
+
+@app.route('/index')
 def index():
     nome= 'ewerton'
     mostrarVideos = True
     lista =['https://www.youtube.com/embed/d35pHYwgVpg','https://www.youtube.com/embed/VTSIrwHUZ-M']
-    return render_template('index.html', nomeHtml = nome, lista = lista,mostrarVideos = mostrarVideos)
+    return render_template('index.html', nomeHtml = nome, lista = lista, mostrarVideos = mostrarVideos)
 
 #app.run(host='0.0.0.0', port=5000)
